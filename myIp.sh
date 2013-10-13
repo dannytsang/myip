5493a35eef0b2b0f9d569b4624537a6d25fda076
Danny Tsang <danny@dannytsang.co.uk>
2013-10-13 23:01:57 +0100

    Added check if the new IP matches the old IP then it will not overwrite the file.
    Using new function to write text to file.

--- myIp.sh
+++ myIp.sh
@@ -3,6 +3,25 @@
 # Load configuration
 source config.sh
 
+source writer.sh
+
 # Get IP
-curl ifconfig.me > $IP_FILE_PATH
+newIp=$(`curl ifconfig.me`)
+
+# Get last IP if any
+if [ -f $IP_FILE_PATH ]
+then
+    read -r oldIp < $IP_FILE_PATH
+    
+    # Check if old and new IP are a match
+    if [ "$newIp" != "$oldIp" ]
+    then
+        echo "New IP: $newIp does not match old IP: $oldIp"
+        writeToFile $newIp
+    fi
+else
+    # No previous IP file found so create one.
+    $(writeToFile $newIp)
+    echo "write to file $newIP 1"
+fi
 
